ad3b6d67ef326f0dc56bcf380a46e888a357991b
tsinat08 <tsinat.weldetnesae@gmail.com>
2019-06-04 22:23:45 -0400

    more link added

--- public/main.js
+++ public/main.js
@@ -30,3 +30,9 @@ const context = {
         }
     ]
 };
+
+const templateElement = document.getElementById("templateHB");
+const templateSource = templateElement.innerHTML;
+const template = Handlebars.compile(templateSource);
+const compiledHtml = template(context);
+document.getElementById('information').innerHTML = compiledHtml;
